ea4843fa943ab93b9e133bdc7d3c07e0f65c6ac5
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-12 11:24:42 -0600

    correccion de rutas

--- app/src/main/java/com/example/petdata/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/petdata/data/network/RetrofitClient.kt
@@ -8,10 +8,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     // En desarrollo usa tu IP local, no localhost
 
+    //Bd-desplada
+    private const val BASE_URL = "https://api-gateway-z8qa.onrender.com/"
     //mi cell
     //private const val BASE_URL = "http://172.20.10.6:3000/"
     //mi house
-    private const val BASE_URL = "http://192.168.1.11:3000/"
+    //private const val BASE_URL = "http://192.168.1.11:3000/"
     //up
     //private const val BASE_URL = "http://10.10.0.55:3000/"
     val apiService: ApiService by lazy {
